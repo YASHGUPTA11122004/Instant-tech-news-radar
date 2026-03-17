ffbfde15040f0fb8efbbc3a8d101291afab46d6c
Yash Gupta <148563569+YASHGUPTA11122004@users.noreply.github.com>
2026-03-17 18:13:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8788",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+    reportCompressedSize: true,
+  },
+});
